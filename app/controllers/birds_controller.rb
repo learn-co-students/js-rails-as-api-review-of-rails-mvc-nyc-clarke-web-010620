a0ebab732ed418042458acf1e3f3dd02fd5fa305
github username <github email address>
2020-02-27 20:33:27 -0500

    Done.

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,1 +1,7 @@
 # Add code from Readme
+
+class CirdsController < ApplicationController
+    def index
+        @birds = Bird.all
+    end
+end 
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   # Add route from Readme
+  get '/bird' => 'birds#index'
 end
